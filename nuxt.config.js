b23f69328a09cfe804a6a0047742c97b816334fb
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-08 08:51:19 +0100

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'vue-uikit-ecommerce-firebase',
+    title: 'vue-uikit-ecommerce-strapi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
